f379c94b58b193c126e5fe1ab46ad4b632e30127
神圣•凯莎 <kaisha@gmail.com>
2024-04-02 12:29:03 +0400

    添加跟高级别的编译参数优化

--- main.cc
+++ main.cc
@@ -7,6 +7,10 @@ int main()
 {
     App::Application().initialization();
 
+
+    // 注册 GzipFilter 插件
+    drogon::app().enableGzip(true);
+
     drogon::app().loadConfigFile("config.json").run();
 
     return 0;
